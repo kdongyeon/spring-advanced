db8e3845cf5182b6c849d7095b0da98ed844306f
kdongyeon <91926630+kdongyeon@users.noreply.github.com>
2025-04-19 14:48:29 +0900

    필수 Lv1

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,9 +1,12 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 
 @Getter
 @NoArgsConstructor
@@ -12,6 +15,12 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
-    @NotBlank
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    @Pattern(regexp = ".*\\d.*", message = "숫자를 포함해야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "대문자를 포함해야합니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
     private String newPassword;
-}
+
+
+    }
+
